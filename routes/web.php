13987305c2d557c796ee47a1ed089ec8343a186b
samue345 <samuef339@gmail.com>
2026-08-29 09:57:04 -0300

    refactor: rename Blade views to English

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,17 @@ use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('analise');
+    return view('credit-analysis');
 });
 
 Route::get('/simulacao/{creditAnalysis}', [SimulationController::class, 'show']);
 Route::get('/contratacoes', [ContractedCreditController::class, 'index']);
 
-Route::view('/clientes', 'clientes.index');
+Route::view('/clientes', 'customers.index');
 Route::get('/clientes/create', function () {
-    return view('clientes.form', ['customer' => null]);
+    return view('customers.form', ['customer' => null]);
 });
 Route::get('/clientes/{customer}/edit', function (Customer $customer) {
-    return view('clientes.form', compact('customer'));
+    return view('customers.form', compact('customer'));
 });
+
