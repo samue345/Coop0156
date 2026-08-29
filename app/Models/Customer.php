7f44c5ed679cf3b7edf6f6255671221406f93c41
samue345 <samuef339@gmail.com>
2026-08-29 16:51:44 -0300

    refactor: clean up customer support code

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasHashidsCode;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory;
     use HasHashidsCode;
 
     protected $table = 'clientes';
