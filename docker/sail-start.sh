659a9ab4a9491f06a1eeb54f83a90f665dd17234
samue345 <samuef339@gmail.com>
2026-08-28 22:44:23 -0300

    fix: enable sail server workers

--- docker/sail-start.sh
+++ docker/sail-start.sh
@@ -8,4 +8,6 @@ touch database/database.sqlite
 
 php artisan migrate --force
 
+export PHP_CLI_SERVER_WORKERS=4
+
 exec /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan serve --host=0.0.0.0 --port=80
